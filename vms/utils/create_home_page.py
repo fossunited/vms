fc2375594494c7775f29573d33d96bacd3666135
Shridhar <shridhar.p@zerodha.com>
2023-02-15 20:59:50 +0530

    fix: web page user profile

--- vms/utils/create_home_page.py
+++ vms/utils/create_home_page.py
@@ -118,10 +118,10 @@ def execute():
         doc.insert(ignore_permissions=True, ignore_links=True)
         frappe.db.commit()
 
-    if not frappe.db.exists("Web Page", "user"):
+    if not frappe.db.exists("Web Page", "user_profile"):
         doc = frappe.get_doc({
             "doctype": "Web Page",
-            "title": "user",
+            "title": "user profile",
             "route": "v/<username>",
             "published": 1,
             "dynamic_route": 1,
