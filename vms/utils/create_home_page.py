75d3935e842298b03c25b6e9c7ceb2b5d3dd7f5c
Shridhar <shridhar.p@zerodha.com>
2023-02-15 18:34:03 +0530

    fix: changed newdoc to get_doc

--- vms/utils/create_home_page.py
+++ vms/utils/create_home_page.py
@@ -6,54 +6,54 @@ import frappe
 def execute():
     """Create home page."""
     if not frappe.db.exists("Web Page", "home"):
-        doc = frappe.new_doc("Web Page")
-        doc.title = "Home"
-        doc.route = "home"
-        doc.published = 1
-        doc.content = "Page Builder"
-        doc.page_blocks = [
-            {
-                "web_template": "Hero with left image",
-                "add_container": 1,
-                "add_top_padding": 1,
-                "add_bottom_padding": 1
-            },
-            {
-                "web_template": "Hero with Right Image",
-                "add_container": 1,
-            },
-            {
-                "web_template": "Top volunteers",
-                "add_container": 1,
-                "add_bottom_padding": 1
-            },
-            {
-                "web_template": "Current Activities",
-                "add_container": 1,
-                "add_bottom_padding": 1
-            },
-            {
-                "web_template": "Past Activities",
-                "add_container": 1,
-                "add_bottom_padding": 1
-            }
-
-        ]
-        doc.context_script = """context.no_cache = 1
-        context.site_map = 1"""
-        doc.javascript = """window.onload = function() {
+        doc = frappe.get_doc({
+            "doctype": "Web Page",
+            "title": "Home",
+            "route": "home",
+            "published": 1,
+            "content": "Page Builder",
+            "page_blocks": [
+                {
+                    "web_template": "Hero with left image",
+                    "add_container": 1,
+                    "add_top_padding": 1,
+                    "add_bottom_padding": 1
+                },
+                {
+                    "web_template": "Hero with Right Image",
+                    "add_container": 1,
+                },
+                {
+                    "web_template": "Top volunteers",
+                    "add_container": 1,
+                    "add_bottom_padding": 1
+                },
+                {
+                    "web_template": "Current Activities",
+                    "add_container": 1,
+                    "add_bottom_padding": 1
+                },
+                {
+                    "web_template": "Past Activities",
+                    "add_container": 1,
+                    "add_bottom_padding": 1
+                }
+            ],
+            "context_script": """context.no_cache = 1
+        context.site_map = 1""",
+            "javascript": """window.onload = function() {
             if (frappe.session.user != 'Guest'){
-                frappe.call({
-                    'method': 'user_mapping',
-                    callback: function(data) {
-                        if(data.message != frappe.session.user){
-                            window.location = '/volunteer-mapping/new'
+                    frappe.call({
+                        'method': 'user_mapping',
+                        callback: function(data) {
+                            if(data.message != frappe.session.user){
+                                window.location = '/volunteer-mapping/new'
+                            }
                         }
-                    }
-                });
-            }
-        };"""
+                    });
+                }
+            };""",
 
-        doc.full_width = 1
-        doc.text_align = "Left"
+            "full_width": 1,
+            "text_align": "Left"})
         doc.save(ignore_permissions=True)
