7d5dd92beb299878afaf3a44be5cfa92df3efa8b
Shridhar <shridhar.p@zerodha.com>
2023-03-31 14:08:54 +0530

    feat: option to register for event

---
+++ vms/vms/doctype/vol_activity_registration/test_vol_activity_registration.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Shridhar Patil and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestVolActivityRegistration(FrappeTestCase):
+	pass
---
+++ vms/vms/doctype/vol_activity_registration/vol_activity_registration.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Shridhar Patil and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Vol Activity Registration", {
+// 	refresh(frm) {
+
+// 	},
+// });
